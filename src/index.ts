e5552bdb1f19490e102f17b6d36a183a27ffec37
TrilochanSahoo <trilochansahoo026@gmail.com>
2024-10-28 23:37:16 +0530

    Added cors middleware

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { Hono } from 'hono'
 import user from '../routes/user'
 import blog from '../routes/blog'
 import auth from '../routes/auth'
+import { cors } from 'hono/cors'
 
 
 const app = new Hono()
 
+app.use('/*', cors())
 app.route('/api/v1/auth',auth)
 app.route('/api/v1/user',user)
 app.route('/api/v1/blog',blog)
