b3650f741876a2ce30d5782b80e64505171bc4c2
TrilochanSahoo <trilochansahoo026@gmail.com>
2024-11-10 20:45:47 +0530

    Oauth api changes added

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { sign, decode } from 'hono/jwt'
 
 const auth = new Hono<{
     Bindings: {
@@ -17,15 +17,30 @@ auth.post('/signup', async(c)=>{
 
     try{
         const body = await c.req.json()
-    
-        const user = await prisma.user.create({
-            data:{
+        let userdata
+        console.log(await decode(body.credential))
+
+        if(!body.externalLogin){
+            userdata = {
                 name : body.name,
                 email : body.email,
                 password : body.password
             }
+        }
+        else{
+            const oauth_cred = await decode(body.credential)
+            console.log(oauth_cred.payload)
+            userdata = {
+                name : oauth_cred.payload.name as string,
+                email : oauth_cred.payload.email as string,
+                externalLogin : body.externalLogin,
+                externalId : body.externalId
+            }
+        }
+        
+        const user = await prisma.user.create({
+            data : userdata
         })
-    
         const payload = {
             id: user.id
         }
@@ -36,6 +51,7 @@ auth.post('/signup', async(c)=>{
             message: "User created successfully",
             auth : token
         })
+    
     }
     catch (error) {
         c.status(411)
@@ -55,12 +71,25 @@ auth.post('/signin', async (c) => {
 
     try {
         const body = await c.req.json()
-    
-        const user = await prisma.user.findFirst({
-            where:{
+
+        let userdata
+
+        if(!body.externalLogin){
+            userdata = {
                 email : body.email,
                 password : body.password
             }
+        }
+        else{
+            userdata = {
+                email : body.email,
+                externalLogin : body.externalLogin,
+                // externalId : body.externalId
+            }
+        }
+
+        const user = await prisma.user.findFirst({
+            where:userdata
         })
 
         if(user!= null){
